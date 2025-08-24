5c972a54d32bb66ba710f4b0cee6594ba5798fb3
Abhishek Pawar <pawarabhishek814@gmail.com>
2025-08-24 10:44:57 +0530

    compressed and optimised solution

--- solution2.js
+++ solution2.js
@@ -3,14 +3,24 @@
 
 //solution :
 
-let array = [1, 2, 3, 3, 4, 5, 6, 6, 2];
-function doublearray(e,index,array) {
-    if (index !== array.length - 1) {
-        return(e * 2);
-    }else{
-        return(e);
-    }
-};
-let doubledarray = array.map(doublearray);
+// let array = [1, 2, 3, 3, 4, 5, 6, 6, 2];
+// function doublearray(e,index,array) {
+//     if (index !== array.length - 1) {
+//         return(e * 2);
+//     }else{
+//         return(e);
+//     }
+// };
+// let doubledarray = array.map(doublearray);
+// console.log(doubledarray);
+
+//optimised solution :
+
+let array = [1, 2, 3, 3, 4, 5, 6, 6, 8];
+let doubledarray = array.map((element, index, array) =>
+    element === array[index + 1] ? element : element * 2);    //if curr element is similar to next element i.e array[index+1] then print e as it is and double the next one ...if we want to double the first one in pair simply change logic of index to index-1;
 console.log(doubledarray);
+
+
+
 
