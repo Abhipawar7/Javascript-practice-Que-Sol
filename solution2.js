65d6ca4b0d7a6391f2276ee2e0451c689a8873fb
Abhishek Pawar <pawarabhishek814@gmail.com>
2025-08-24 11:10:06 +0530

    solved real world example based question

--- solution2.js
+++ solution2.js
@@ -21,6 +21,29 @@ let doubledarray = array.map((element, index, array) =>
     element === array[index + 1] ? element : element * 2);    //if curr element is similar to next element i.e array[index+1] then print e as it is and double the next one ...if we want to double the first one in pair simply change logic of index to index-1;
 console.log(doubledarray);
 
+//implementation in real world example :
 
+//function for chat app . if a user sends multiple consecutive msgs,so without showing the avatar again just show msgs:
+// 👤 Alex:  Hey!
+//           How are you?
+//           Did you see the match?
 
+// 👤 Sam:   Yeah, crazy game!
+
+// solution :
+const messages = [
+    { user: "Alex", text: "Hey!" },
+    { user: "Alex", text: "How are you?" },
+    { user: "Alex", text: "Did you see the match?" },
+    { user: "Sam", text: "Yeah crazy game!" },
+
+];
+const formattedmessages = messages.map((msg, index, array) => {
+    if (index === 0 || msg.user !== array[index - 1].user) {
+        return `👤  ${msg.user}:${msg.text}`;
+    } else {
+        return `    ${msg.text}`;
+    }
+});
+console.log(formattedmessages.join("\n"));
 
